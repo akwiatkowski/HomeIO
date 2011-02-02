82207954e9e65b1400ab0e6e83bf88b5795a5d86
Aleksander Kwiatkowski <bobikx@poczta.fm>
2011-02-02 19:50:49 +0100

    creating new rails app

---
+++ trunk/rails_app/home_io/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HomeIo::Application.config.secret_token = '0b94b6f4b12562680b0a416619d3a7e4d18dd0cf1aa648274beb32c78f48d3ffd2ef9fb2e9bfd3eb5d8bcb50b6ed1979cd5cdda7b18217b753a43ab142b3a577'
