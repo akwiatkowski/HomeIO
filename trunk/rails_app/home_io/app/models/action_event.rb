510af23fc718074f720eea24a78ca2d03ab9e3df
Aleksander Kwiatkowski <bobikx@poczta.fm>
2011-03-11 00:26:51 +0100

    rails models for actions

---
+++ trunk/rails_app/home_io/app/models/action_event.rb
@@ -0,0 +1,34 @@
+#!/usr/bin/ruby
+#encoding: utf-8
+
+# HomeIO - home control system.
+# Copyright (C) 2011 Aleksander Kwiatkowski
+#
+# This file is part of HomeIO.
+#
+# HomeIO is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# HomeIO is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with HomeIO.  If not, see <http://www.gnu.org/licenses/>.
+
+
+# Events of actions executions
+
+class ActionEvent < ActiveRecord::Base
+  belongs_to :action_type
+
+  validates_presence_of :time
+
+  # will paginate
+  cattr_reader :per_page
+  @@per_page = 20
+
+end
