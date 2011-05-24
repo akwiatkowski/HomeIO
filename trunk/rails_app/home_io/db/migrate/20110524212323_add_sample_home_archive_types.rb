d8c1a322c4c38495bcf35798fb69a94ebafb61a4
Aleksander Kwiatkowski <bobikx@poczta.fm>
2011-05-25 00:15:32 +0200

    fix

--- trunk/rails_app/home_io/db/migrate/20110524212323_add_sample_home_archive_types.rb
+++ trunk/rails_app/home_io/db/migrate/20110524212323_add_sample_home_archive_types.rb
@@ -2,9 +2,14 @@ class AddSampleHomeArchiveTypes < ActiveRecord::Migration
   def self.up
     HomeArchiveType.transaction do
       # sample parameters
-      HomeArchiveType.find_or_create_by_name("total_grid_power_consumed")
-      HomeArchiveType.find_or_create_by_name("furnace_fuel_consumed")
-      HomeArchiveType.find_or_create_by_name("furnace_unprocessed_output")
+      #HomeArchiveType.find_or_create_by_name("total_grid_power_consumed")
+      #HomeArchiveType.find_or_create_by_name("furnace_fuel_consumed")
+      #HomeArchiveType.find_or_create_by_name("furnace_unprocessed_output")
+
+      ["total_grid_power_consumed", "furnace_fuel_consumed", "furnace_unprocessed_output"].each do |name|
+        HomeArchiveType.create!(:name => name) if HomeArchiveType.find(:first, :conditions => { :name => name }).nil?
+      end
+
     end
   end
 
