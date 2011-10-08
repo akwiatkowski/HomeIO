32b15368eb53b915b6da32f2fbf2299c515cf2e9
Aleksander Kwiatkowski <bobikx@poczta.fm>
2011-10-08 17:54:44 +0200

    watchdog - completed

--- trunk/ruby_server/lib/backend_watchdog.rb
+++ trunk/ruby_server/lib/backend_watchdog.rb
@@ -19,6 +19,7 @@
 # You should have received a copy of the GNU General Public License
 # along with HomeIO.  If not, see <http://www.gnu.org/licenses/>.
 
+require 'lib/utils/adv_log'
 require 'lib/communication/tcp/tcp_comm_protocol'
 require 'lib/communication/task_server/tcp_task'
 require 'lib/utils/config_loader'
@@ -30,7 +31,6 @@ require 'lib/storage/active_record/backend_models/weather_metar_archive'
 # They tend to freeze every week, I know this issue but it is hard to analyze
 # what is wrong.
 
-dir_path = "data/pid/"
 
 class BackendWatchdog
 
@@ -46,17 +46,45 @@ class BackendWatchdog
   # right to kill
   ARMED = true
 
+  # where are pid files
+  DIR_PATH = "data/pid/"
+
   def initialize
     @tcp_config = ConfigLoader.instance.config('TcpCommTaskServer')
     @tcp_port = @tcp_config[:port]
 
     @storage_ar = StorageActiveRecord.instance
+
+    @logger = AdvLog.instance.logger(self)
+  end
+
+  def scan_pids
+    pids = get_pids
+
+    pids.each do |p|
+      if p[:file] =~ /control/
+        txt = "Checking control"
+        puts txt
+        @logger.debug(txt)
+
+        kill_pid(p) if check_control_backend
+      end
+
+      if p[:file] =~ /weather/
+        txt = "Checking weather"
+        puts txt
+        @logger.debug(txt)
+
+        kill_pid(p) if check_weather_backend
+      end
+
+    end
   end
 
   def get_pids
     a = Array.new
 
-    d = Dir.glob("#{dir_path}*.pid").each do |file_name|
+    d = Dir.glob("#{DIR_PATH}*.pid").each do |file_name|
       pid_file = File.new(file_name, "r")
       pid = pid_file.readline
       pid_file.close
@@ -67,7 +95,22 @@ class BackendWatchdog
       }
     end
 
-    return pid
+    return a
+  end
+
+  def kill_pid(p)
+    # safe mode
+    return unless ARMED
+
+    pid = p[:pid]
+    file_name = p[:file]
+
+    txt = "Killing #{file_name} - PID  #{pid}"
+    puts txt
+    @logger.warn(txt)
+
+    `kill -9 #{pid}`
+    `rm #{file_name}`
   end
 
   # Check if latest WeatherArchive was updated later than 12 hours ago
@@ -79,20 +122,30 @@ class BackendWatchdog
     a = WeatherArchive.order("id DESC").limit(20).all
     time = a.collect { |w| w.updated_at }.min
     time_d = Time.now - time
-    puts "Weather interval = #{time_d}" if VERBOSE
+    txt = "Weather interval = #{time_d} (#{(time_d / 60).to_i} min, #{(time_d / 3600).to_i} h, threshold #{MEASUREMENTS_TIMEOUT})"
+    puts txt if VERBOSE
+    @logger.debug(txt)
 
     if time_d > WEATHER_TIMEOUT
-      puts "Weather backend timeout"
+      txt = "Weather backend timeout"
+      puts txt
+      @logger.warn(txt)
+
       return true
     end
 
     a = WeatherMetarArchive.order("id DESC").limit(20).all
     time = a.collect { |w| w.updated_at }.min
     time_d = Time.now - time
-    puts "Weather (metar) interval = #{time_d}" if VERBOSE
+    txt = "Weather (metar) interval = #{time_d} (#{(time_d / 60).to_i} min, #{(time_d / 3600).to_i} h, threshold #{MEASUREMENTS_TIMEOUT})"
+    puts txt if VERBOSE
+    @logger.debug(txt)
 
     if time_d > WEATHER_TIMEOUT
-      puts "Weather backend timeout"
+      txt = "Weather backend timeout"
+      puts txt
+      @logger.warn(txt)
+
       return true
     end
 
@@ -117,10 +170,16 @@ class BackendWatchdog
     time = measurements_times.min
     time_d = Time.now - time
 
-    puts "Control measurements interval = #{time_d}" if VERBOSE
+    txt = "Control measurements interval = #{time_d} (#{(time_d / 60).to_i} min, #{(time_d / 3600).to_i} h, threshold #{MEASUREMENTS_TIMEOUT})"
+    puts txt if VERBOSE
+    @logger.debug(txt)
+
 
     if time_d > MEASUREMENTS_TIMEOUT
-      puts "Control backend timeout"
+      txt = "Control backend timeout"
+      puts txt if VERBOSE
+      @logger.warn(txt)
+      
       return true
     else
       return false
@@ -131,6 +190,4 @@ class BackendWatchdog
 end
 
 b = BackendWatchdog.new
-#puts b.check_control_backend.inspect
-puts b.check_weather_backend
-
+b.scan_pids
