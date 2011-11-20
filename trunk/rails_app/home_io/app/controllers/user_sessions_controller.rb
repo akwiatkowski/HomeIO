b1fe77570e927f71d951d7ec6a4e4a868fb664a7
Aleksander Kwiatkowski <bobikx@poczta.fm>
2011-11-20 21:18:45 +0100

    redirect to root after logout

--- trunk/rails_app/home_io/app/controllers/user_sessions_controller.rb
+++ trunk/rails_app/home_io/app/controllers/user_sessions_controller.rb
@@ -30,7 +30,8 @@ class UserSessionsController < ApplicationController
   def destroy
     current_user_session.destroy
     flash[:notice] = "Logout successful!"
-    redirect_back_or_default new_user_session_url
+    #redirect_back_or_default new_user_session_url
+    redirect_to root_path
   end
 
 end
