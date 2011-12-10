677a748b5a861cc25471a3072fbbca8d19d7c014
Aleksander Kwiatkowski <bobikx@poczta.fm>
2011-12-10 11:57:09 +0100

    using Pry

--- trunk/rails_app/home_io/config/environments/development.rb
+++ trunk/rails_app/home_io/config/environments/development.rb
@@ -10,7 +10,7 @@ HomeIo::Application.configure do
   config.whiny_nils = true
 
   # Show full error reports and disable caching
-  config.consider_all_requests_local       = true
+  config.consider_all_requests_local = true
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send
@@ -21,5 +21,14 @@ HomeIo::Application.configure do
 
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
+
+  # using Pry
+  silence_warnings do
+    begin
+      require 'pry'
+      IRB = Pry
+    rescue LoadError
+    end
+  end
 end
 
