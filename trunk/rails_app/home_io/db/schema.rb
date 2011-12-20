3f65e21f76921d09d1758d38eeebc6c954feb80f
Aleksander Kwiatkowski <bobikx@poczta.fm>
2011-12-20 09:10:19 +0100

    fix

--- trunk/rails_app/home_io/db/schema.rb
+++ trunk/rails_app/home_io/db/schema.rb
@@ -175,6 +175,7 @@ ActiveRecord::Schema.define(:version => 20111210132450) do
     t.string   "encrypted_password",     :limit => 128, :default => "",    :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
+    t.string   "remember_token"
     t.datetime "remember_created_at"
     t.integer  "sign_in_count",                         :default => 0
     t.datetime "current_sign_in_at"
