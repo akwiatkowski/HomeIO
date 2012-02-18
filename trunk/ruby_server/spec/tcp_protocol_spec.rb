ad29947cf1c792e0cf362bbbf60859b0bf92af55
Aleksander Kwiatkowski <bobikx@poczta.fm>
2012-02-18 20:52:39 +0100

    am i writing a spec?

---
+++ trunk/ruby_server/spec/tcp_protocol_spec.rb
@@ -0,0 +1,14 @@
+require './lib/communication/tcp_comm_protocol.rb'
+
+describe "TcpProtocol" do
+  context 'simple' do
+    before(:each) do
+      #@a = 1
+    end
+
+    it "should" do
+      @a.should == 1
+    end
+  end
+
+end
