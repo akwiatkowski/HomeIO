fae1a6df48720426e4d1efc47d5b274bd340e199
Aleksander Kwiatkowski <bobikx@poczta.fm>
2012-03-18 12:16:02 +0100

    new server (begin)

---
+++ trunk/server/README.txt
@@ -0,0 +1,1 @@
+New experimental server for HomeIO
---
+++ trunk/server/lib/home_io_server.rb
@@ -0,0 +1,7 @@
+$:.unshift(File.dirname(__FILE__))
+
+require 'simple_metar_parser/metar'
+require 'simple_metar_parser/parser'
+
+module HomeIoServer
+end
