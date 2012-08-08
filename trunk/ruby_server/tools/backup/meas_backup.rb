d29c6983421e9b51703b9af315fa1b22136ba0e8
Aleksander Kwiatkowski <bobikx@poczta.fm>
2012-08-08 21:52:01 +0200

    new meas backup tool

---
+++ trunk/ruby_server/tools/backup/meas_backup.rb
@@ -0,0 +1,37 @@
+require 'rubygems'
+require 'yaml'
+require './lib/storage/storage_active_record.rb'
+
+Dir.mkdir('data/backup') unless File.exists?('data/backup')
+s = StorageActiveRecord.instance
+
+puts "getting all meas types"
+types = MeasType.all
+
+puts "getting first meas"
+first_meas = MeasArchive.order(:time_from).first
+puts "... first meas time #{first_meas.time_from}"
+
+time = first_meas.time_from.beginning_of_day
+time_limit = Time.now.end_of_day
+
+puts "#{(time_limit - time) / 1.days} days to go"
+
+while time < time_limit
+  puts "starting day #{time}"
+  bt = Time.now
+
+  file_name = File.join('data', 'backup', "meas_#{time.strftime("%Y_%m_%d")}.csv")
+  file = File.new(file_name, 'w')
+  file.puts "meas_type;unix_time_from;unix_time_to;raw_value;value"
+
+  meases = MeasArchive.where(["time_from >= ?", time]).where(["time_from < ?", time + 1.day]).all
+  meases.each do |m|
+    file.puts "#{types.select{|t| t.id == m.meas_type.id}.first.name};#{m.time_from.to_f};#{m.time_to.to_f};#{m.raw};#{m.value}"
+  end
+
+  file.close
+
+  puts "finished day #{time}, took #{Time.now - bt}, meases #{meases.size}"
+  time += 1.day
+end
