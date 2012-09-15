4e7f52ddb2a81543ae720bd6a38f04f2a463bd8a
Aleksander Kwiatkowski <bobikx@poczta.fm>
2012-09-15 14:51:34 +0200

    cities list

--- trunk/server/lib/home_io_server.rb
+++ trunk/server/lib/home_io_server.rb
@@ -1,5 +1,7 @@
 $:.unshift(File.dirname(__FILE__))
 
+require 'rufus/scheduler'
+
 require 'home_io_server/redis_proxy'
 require 'home_io_server/meas_server'
 require 'home_io_server/weather_server'
--- trunk/server/lib/home_io_server/weather_server.rb
+++ trunk/server/lib/home_io_server/weather_server.rb
@@ -1,9 +1,15 @@
+require "weather_fetcher"
+
 # Server fetching weather
 
 module HomeIoServer
   class WeatherServer
 
     def initialize
+      @scheduler = Rufus::Scheduler.start_new
+      @scheduler.every '1s' do
+        puts 'check blood pressure'
+      end
     end
 
   end
